c1f8d6614decf4fdd65cfdb76753604bebe901ea
Tyler Collier <tyler.collier+github@devmounta.in>
2018-03-22 10:40:19 -0700

    Added app component

---
+++ components/app.js
@@ -0,0 +1,8 @@
+angular.module('themechanger').component('app', {
+  templateUrl: 'templates/app.html',
+  controller: function() {
+    this.allowEdit = true;
+    this.fontColor = 'black';
+  },
+  controllerAs: 'myCtrl',
+});
